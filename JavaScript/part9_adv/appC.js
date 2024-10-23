97885a64a2d8c0513322740499d577e4a6e5c12c
Saadkhan0105 <saadkhan9560@gmail.com>
2024-10-23 16:34:54 +0530

    Exporting and Importing Common JS and Module JS

---
+++ JavaScript/part9_adv/appC.js
@@ -0,0 +1,5 @@
+const mathOperations = require("./mathOperationC.js");
+
+console.log(mathOperations.add(10, 10));
+console.log(mathOperations.subtract(10, 5));
+console.log(mathOperations.multiply(10, 10));
---
+++ JavaScript/part9_adv/appM.js
@@ -0,0 +1,13 @@
+// default import
+
+import { multiply } from "./mathOperationsM.js";
+
+// named import
+
+import { add, subtract } from "./mathOperationsM.js";
+
+console.log(add(5, 10));
+
+console.log(subtract(50, 10));
+
+console.log(multiply(8, 10));
